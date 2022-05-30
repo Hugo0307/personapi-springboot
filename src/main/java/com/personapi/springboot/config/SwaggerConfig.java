e7c99dcd5ba7a93ff82eedf31bf2319b724f7f3d
hhenrique <hugohenrique20@gmail.com>
2022-05-30 18:12:25 -0300

    adicionando o Swagger ao projeto, testes dos end-points ok

---
+++ src/main/java/com/personapi/springboot/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.personapi.springboot.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public Docket personApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.personapi.springboot"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(metaInfo());
+	}
+	
+	private ApiInfo metaInfo() {
+		ApiInfo apiInfo = new ApiInfo("Person API Spring Boot",
+				"1.0",
+				"Sistema de gerenciamento de pessoas em uma API REST com Spring Boot",
+				"Terms of Service",
+				new Contact("Hugo Almeida", "https://www.linkedin.com/in/hugo-almeida-72683488/", "developerhugoh@gmail.com"),
+				"Apache License Version 2.0",
+				"https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+				);
+		return apiInfo;
+				
+	}
+	
+}
--- src/main/java/com/personapi/springboot/controller/PessoaController.java
+++ src/main/java/com/personapi/springboot/controller/PessoaController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,13 @@ import com.personapi.springboot.dto.PessoaDTO;
 import com.personapi.springboot.exception.PessoaNotFoundException;
 import com.personapi.springboot.service.PessoaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/v1/people")
+@Api(value = "API REST Pessoa")
+@CrossOrigin(origins = "*")
 //@AllArgsConstructor(onConstructor = @__(@Autowired))//cria um construtor default em tempo de compilação como o que está abaixo comentado
 public class PessoaController {
 	
@@ -33,23 +39,27 @@ public class PessoaController {
 		this.pessoaService = pessoaService;
 	}
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public MessageResponseDTO createPessoa(@RequestBody @Valid PessoaDTO pessoaDTO) {
-		return pessoaService.createPessoa(pessoaDTO);
-	}
-	
 	@GetMapping
+	@ApiOperation(value = "retorna os dados de uma lista de pessoas")
 	public List<PessoaDTO> listAll() {
 		return pessoaService.listAll();
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "retorna os dados de uma pessoa")
 	public PessoaDTO findById(@PathVariable Long id) throws PessoaNotFoundException {
 		return pessoaService.findById(id);
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "salva os dados de uma pessoa")
+	public MessageResponseDTO createPessoa(@RequestBody @Valid PessoaDTO pessoaDTO) {
+		return pessoaService.createPessoa(pessoaDTO);
+	}
+		
 	@PutMapping("/{id}")
+	@ApiOperation(value = "atualiza os dados de uma pessoa")
 	public MessageResponseDTO updateById(@PathVariable Long id, 
 			@RequestBody @Valid PessoaDTO pessoaDTO) throws PessoaNotFoundException {
 		return pessoaService.updateById(id, pessoaDTO);
@@ -57,6 +67,7 @@ public class PessoaController {
 	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "deleta os dados de uma pessoa")
 	public void deleteById(@PathVariable Long id) throws PessoaNotFoundException {
 		pessoaService.delete(id);
 	}
